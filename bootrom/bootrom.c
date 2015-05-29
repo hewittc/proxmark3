393d57bbe7c0ad3b7d4bdbbf99069ad136116a1f
Christopher Hewitt <hewittc@users.noreply.github.com>
2015-05-28 21:10:06 -0400

    Set flash wait state for second bank of memory on 512k part. Omitting it causes processor to halt on startup.

--- bootrom/bootrom.c
+++ bootrom/bootrom.c
@@ -283,6 +283,9 @@ void BootROM(void)
 	AT91C_BASE_EFC0->EFC_FMR =
 		AT91C_MC_FWS_1FWS |
 		MC_FLASH_MODE_MASTER_CLK_IN_MHZ(48);
+	AT91C_BASE_EFC1->EFC_FMR =
+		AT91C_MC_FWS_1FWS |
+		MC_FLASH_MODE_MASTER_CLK_IN_MHZ(48);
 
     // Initialize all system clocks
     ConfigClocks();
